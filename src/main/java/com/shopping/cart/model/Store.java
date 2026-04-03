a98152cb2ade969272fe5cf9d0f9244eef6c35b9
AkashVandu <akashvandu.as@gmail.com>
2026-04-04 00:03:33 +0530

    New Model Added by: Vandana

---
+++ src/main/java/com/shopping/cart/model/Store.java
@@ -0,0 +1,33 @@
+package com.shopping.cart.model;
+
+public class Store {
+    private String storeName;
+    private String storeLocation;
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getStoreLocation() {
+        return storeLocation;
+    }
+
+    public void setStoreLocation(String storeLocation) {
+        this.storeLocation = storeLocation;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    private int storeId;
+
+}
